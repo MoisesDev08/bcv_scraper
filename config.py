9e569be50f9d90ca346e1859177134eec2f8633e
MoisesDev08 <moises08.dev@gmail.com>
2026-06-03 15:46:24 -0400

    Estructura inicial del proyecto

---
+++ config.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+# --- DIRS ---
+
+BASE_DIR = Path(__file__).parent
+CERT_DIR = BASE_DIR / "cert"
+DATA_DIR = BASE_DIR / "data"
+XLS_HISTORY_DIR = DATA_DIR / "xls_files_history"
+SCRAPER_DIR = BASE_DIR / "scraper"
+HTMLS_DIR = DATA_DIR / "htmls"
+TESTS_DIR = BASE_DIR / "tests"
+
+# --- PATHS ---
+
+CERT_PATH = CERT_DIR / "bcv_org.pem"
+DEBUGGING_BASE_INDEX_PATH = HTMLS_DIR / "index.html" # html base
+DEBUGGING_XLS_INDEX_PATH = HTMLS_DIR / "links_index.html" # Página con los links, tiene paginación
+
+URL_BASE = "https://www.bcv.org.ve/"
+URL_WITH_XLS_FILES = "https://www.bcv.org.ve/estadisticas/tipo-cambio-de-referencia-smc"
+
+# --- PARSER SELECTORS ---
+
+DOLAR_RATE_SELECTOR = "div#dolar strong.strong-tb" 
+TABLE_SELECTOR = "div.view-content > table.views-table.cols-2.table.table-0"
+
+# DEBE SER USADO EN EL TAG "TABLE"
+LINK_SELECTOR = "tbody tr span.file a[href$='.xls']" # revisar validez
